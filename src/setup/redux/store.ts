0998ca69f9029c4b75e35a1111da56e8bb73b520
yasamanbarati <yasamanbarati81@gmail.com>
2024-10-25 20:39:36 +0330

    feat: development redux structure(hooks&store)

--- src/setup/redux/store.ts
+++ src/setup/redux/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import coinReducer from "scenes/_slice/coin.slice";
-import exchangeReducer from "scenes/_slice/exchange.slice";
 
 export const store = configureStore({
   reducer: {
     coin: coinReducer,
-    exchange: exchangeReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
